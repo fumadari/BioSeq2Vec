ef4d3ea9514394b98296d3a68312f0a65b4eb9c5
fumadari <dariofumarola90@gmail.com>
2024-10-04 23:25:27 -0400

    Refactored and finished skeleton

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import torch
+from train import train_model
+from model import SeqTransformer
+import yaml
+import os
+from data_loader import SequenceDataset
+
+if __name__ == "__main__":
+    # Load configuration
+    with open(os.path.join('configs', 'config.yaml'), 'r') as config_file:
+        config = yaml.safe_load(config_file)
+    
+    # Prepare data
+    dataset = SequenceDataset(config['data']['sequences_file'])
+    
+    # Initialize model
+    model = SeqTransformer(config['model'])
+    
+    # Train model
+    train_model(model, dataset, config)
